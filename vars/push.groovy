dbb01f5c74b59cab9ad8d3a26cc5651c6dacf9f7
DhirajPatil98 <patildhiraj523@gmail.com>
2025-10-31 16:05:18 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -2,7 +2,7 @@ def call(String project, String imageTag)
 {
 echo 'pushing code to docker hub'
                 withCredentials([usernamePassword(credentialsId:'dockerHubCreds', passwordVariable:"dockerHubPass", usernameVariable:"dockerHubUser")]){
-                sh "docker tag ${project}:${imageTag} ${env.dockerHubUser}/notes-app:latest"
+                sh "docker tag ${project}:${imageTag} ${env.dockerHubUser}/${project}:${imageTag}"
                 sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
                 sh "docker push ${env.dockerHubUser}/${project}:${imageTag}"
                 }
